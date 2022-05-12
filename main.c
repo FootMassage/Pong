a586aec6c9ca6555a25edffe573a51655e209bcb
Tato <tato@pop-os.localdomain>
2022-05-12 05:57:18 -0300

    first commit

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <GL/freeglut.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define WHEIGHT 720
 #define WWIDTH 1080
+
+bool isCollidingPalet1(float x, float y, float *rectv, float *rectv2)
+{
+    return (x == rectv[0] && y >= rectv[1] && y <= rectv2[1]) ? true : false;
+}
+bool isCollidingPalet2(float x, float y, float *rectv, float *rectv2)
+{
+    return (x == rectv[0] && y <= rectv[1] && y >= rectv2[1]) ? true : false;
+}
+float x = 0, y = 0;
+float rect1v[] = {9, -2}, rect1v2[] = {9.5, 2}, rect2v[] = {-9, 2}, rect2v2[] = {-9.5, -2};
+float speed = 0.125;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -11,13 +24,23 @@ void display()
 
     // draw
     glPointSize(30.0);
+    glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_POINTS);
-    glVertex2f(0, 0);
+    glVertex2f(x, y);
     glEnd();
-    glRectf(8, -4, 9, 4);
-    glRectf(-8, 4, -9, -4);
 
-    glFlush();
+    x += speed;
+    if (isCollidingPalet1(x, y, rect1v, rect1v2) || isCollidingPalet2(x, y, rect2v, rect2v2))
+    {
+        speed = -speed;
+    }
+
+    glColor3f(1.0, 0.0, 0.0);
+    glRectfv(rect1v, rect1v2);
+    glColor3f(0.0, 0.0, 1.0);
+    glRectfv(rect2v, rect2v2);
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
 void init()
 {
@@ -31,20 +54,22 @@ void reshape(int w, int h)
     gluOrtho2D(-10, 10, -10, 10);
     glMatrixMode(GL_MODELVIEW);
 }
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
     glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - WWIDTH) / 2, (glutGet(GLUT_SCREEN_HEIGHT) - WHEIGHT) / 2);
 
     glutInitWindowSize(WWIDTH, WHEIGHT);
 
-    glutCreateWindow("XD");
+    glutCreateWindow("Pong");
     init();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
 
     glutMainLoop();
+
     return EXIT_SUCCESS;
 }
