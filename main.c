7ab7ab95bb13b4119991358f387615314e4dd897
Tato <tato@pop-os.localdomain>
2022-05-12 15:37:23 -0300

    Checkeo de colisiones

--- main.c
+++ main.c
@@ -5,32 +5,75 @@
 
 #define WHEIGHT 720
 #define WWIDTH 1080
+/*Declaracion de variables globales*/
+GLfloat x = 0, y = 0;
+GLfloat rect1v[] = {9, -2}, rect1v2[] = {9.5, 2}, rect2v[] = {-9, 2}, rect2v2[] = {-9.5, -2};
+GLfloat speed = 0.125;
+GLfloat pointSize = 30.0;
+GLint redScore = 0;
+GLint blueScore = 0;
+/*Checkeo de colisiones*/
+bool isCollidingPalet1()
+{
+    return (x >= rect1v[0] - (pointSize / 100) && y >= rect1v[1] - (pointSize / 100) && y <= rect1v2[1] + (pointSize / 100));
+}
+bool isCollidingPalet2()
+{
+    return (x <= rect2v[0] + (pointSize / 100) && y <= rect2v[1] + (pointSize / 100) && y >= rect2v2[1] - (pointSize / 100));
+}
+bool isCollidingWalls()
+{
+    return (y == 10 || y == -10);
+}
+bool redScored()
+{
+    return (x <= -10);
+}
+bool blueScored()
+{
+    return (x >= 10);
+}
+/*Checkeo de goles*/
+void checkGoals()
+{
+    if (redScored())
+    {
+        redScore++;
+        x = 0;
+        y = 0;
+        printf("Red score= %d\n", redScore);
+    }
+    if (blueScored())
+    {
+        blueScore++;
+        x = 0;
+        y = 0;
+        printf("Blue score= %d\n", blueScore);
+    }
+}
+/*Movimiento de las paletas*/
+/*void checkMovP1()
+{
 
-bool isCollidingPalet1(float x, float y, float *rectv, float *rectv2)
-{
-    return (x == rectv[0] && y >= rectv[1] && y <= rectv2[1]) ? true : false;
-}
-bool isCollidingPalet2(float x, float y, float *rectv, float *rectv2)
-{
-    return (x == rectv[0] && y <= rectv[1] && y >= rectv2[1]) ? true : false;
-}
-float x = 0, y = 0;
-float rect1v[] = {9, -2}, rect1v2[] = {9.5, 2}, rect2v[] = {-9, 2}, rect2v2[] = {-9.5, -2};
-float speed = 0.125;
+}*/
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
+    checkGoals();
 
     // draw
-    glPointSize(30.0);
+    glPolygonMode(GL_FRONT, GL_LINE);
+    glLineWidth(2);
+    glPointSize(pointSize);
     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_POINTS);
     glVertex2f(x, y);
     glEnd();
 
     x += speed;
-    if (isCollidingPalet1(x, y, rect1v, rect1v2) || isCollidingPalet2(x, y, rect2v, rect2v2))
+    y += speed / 2;
+    if (isCollidingPalet1() || isCollidingPalet2() || isCollidingWalls())
     {
         speed = -speed;
     }
@@ -42,6 +85,7 @@ void display()
     glutSwapBuffers();
     glutPostRedisplay();
 }
+
 void init()
 {
     glClearColor(0.0, 0.0, 0.0, 0.0);
